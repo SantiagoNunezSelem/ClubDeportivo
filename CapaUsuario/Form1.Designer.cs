c4c43b3d3234e305f8a200e5804385f4fb6e6851
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-16 18:49:31 -0300

    Agregar archivos de proyecto.

---
+++ CapaUsuario/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CapaUsuario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sociosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pagosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actividadesDeportivasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarUnNuevoSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarInformaciónSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darDeBajaSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarPagoDeSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónPagosDeSocioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarActividadDeportivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaciónDeActividadDeportivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarActividadDeportivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarActividadDeportivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sociosToolStripMenuItem,
+            this.pagosToolStripMenuItem,
+            this.actividadesDeportivasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sociosToolStripMenuItem
+            // 
+            this.sociosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarUnNuevoSocioToolStripMenuItem,
+            this.informaciónSocioToolStripMenuItem,
+            this.editarInformaciónSocioToolStripMenuItem,
+            this.darDeBajaSocioToolStripMenuItem});
+            this.sociosToolStripMenuItem.Name = "sociosToolStripMenuItem";
+            this.sociosToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.sociosToolStripMenuItem.Text = "Socios";
+            // 
+            // pagosToolStripMenuItem
+            // 
+            this.pagosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarPagoDeSocioToolStripMenuItem,
+            this.informaciónPagosDeSocioToolStripMenuItem});
+            this.pagosToolStripMenuItem.Name = "pagosToolStripMenuItem";
+            this.pagosToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.pagosToolStripMenuItem.Text = "Pagos";
+            // 
+            // actividadesDeportivasToolStripMenuItem
+            // 
+            this.actividadesDeportivasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarActividadDeportivaToolStripMenuItem,
+            this.informaciónDeActividadDeportivaToolStripMenuItem,
+            this.editarActividadDeportivaToolStripMenuItem,
+            this.eliminarActividadDeportivaToolStripMenuItem});
+            this.actividadesDeportivasToolStripMenuItem.Name = "actividadesDeportivasToolStripMenuItem";
+            this.actividadesDeportivasToolStripMenuItem.Size = new System.Drawing.Size(139, 20);
+            this.actividadesDeportivasToolStripMenuItem.Text = "Actividades Deportivas";
+            // 
+            // agregarUnNuevoSocioToolStripMenuItem
+            // 
+            this.agregarUnNuevoSocioToolStripMenuItem.Name = "agregarUnNuevoSocioToolStripMenuItem";
+            this.agregarUnNuevoSocioToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.agregarUnNuevoSocioToolStripMenuItem.Text = "Agregar un nuevo Socio";
+            // 
+            // informaciónSocioToolStripMenuItem
+            // 
+            this.informaciónSocioToolStripMenuItem.Name = "informaciónSocioToolStripMenuItem";
+            this.informaciónSocioToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.informaciónSocioToolStripMenuItem.Text = "Información Socio";
+            // 
+            // editarInformaciónSocioToolStripMenuItem
+            // 
+            this.editarInformaciónSocioToolStripMenuItem.Name = "editarInformaciónSocioToolStripMenuItem";
+            this.editarInformaciónSocioToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.editarInformaciónSocioToolStripMenuItem.Text = "Editar información Socio";
+            // 
+            // darDeBajaSocioToolStripMenuItem
+            // 
+            this.darDeBajaSocioToolStripMenuItem.Name = "darDeBajaSocioToolStripMenuItem";
+            this.darDeBajaSocioToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.darDeBajaSocioToolStripMenuItem.Text = "Dar de baja Socio";
+            // 
+            // registrarPagoDeSocioToolStripMenuItem
+            // 
+            this.registrarPagoDeSocioToolStripMenuItem.Name = "registrarPagoDeSocioToolStripMenuItem";
+            this.registrarPagoDeSocioToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.registrarPagoDeSocioToolStripMenuItem.Text = "Registrar Pago de Socio";
+            // 
+            // informaciónPagosDeSocioToolStripMenuItem
+            // 
+            this.informaciónPagosDeSocioToolStripMenuItem.Name = "informaciónPagosDeSocioToolStripMenuItem";
+            this.informaciónPagosDeSocioToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.informaciónPagosDeSocioToolStripMenuItem.Text = "Información Pagos de Socio";
+            // 
+            // agregarActividadDeportivaToolStripMenuItem
+            // 
+            this.agregarActividadDeportivaToolStripMenuItem.Name = "agregarActividadDeportivaToolStripMenuItem";
+            this.agregarActividadDeportivaToolStripMenuItem.Size = new System.Drawing.Size(262, 22);
+            this.agregarActividadDeportivaToolStripMenuItem.Text = "Agregar Actividad Deportiva";
+            // 
+            // informaciónDeActividadDeportivaToolStripMenuItem
+            // 
+            this.informaciónDeActividadDeportivaToolStripMenuItem.Name = "informaciónDeActividadDeportivaToolStripMenuItem";
+            this.informaciónDeActividadDeportivaToolStripMenuItem.Size = new System.Drawing.Size(262, 22);
+            this.informaciónDeActividadDeportivaToolStripMenuItem.Text = "Información de Actividad Deportiva";
+            // 
+            // editarActividadDeportivaToolStripMenuItem
+            // 
+            this.editarActividadDeportivaToolStripMenuItem.Name = "editarActividadDeportivaToolStripMenuItem";
+            this.editarActividadDeportivaToolStripMenuItem.Size = new System.Drawing.Size(262, 22);
+            this.editarActividadDeportivaToolStripMenuItem.Text = "Editar Actividad Deportiva";
+            // 
+            // eliminarActividadDeportivaToolStripMenuItem
+            // 
+            this.eliminarActividadDeportivaToolStripMenuItem.Name = "eliminarActividadDeportivaToolStripMenuItem";
+            this.eliminarActividadDeportivaToolStripMenuItem.Size = new System.Drawing.Size(262, 22);
+            this.eliminarActividadDeportivaToolStripMenuItem.Text = "Eliminar Actividad Deportiva";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sociosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarUnNuevoSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarInformaciónSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darDeBajaSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pagosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarPagoDeSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónPagosDeSocioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actividadesDeportivasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarActividadDeportivaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaciónDeActividadDeportivaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarActividadDeportivaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarActividadDeportivaToolStripMenuItem;
+    }
+}
+
