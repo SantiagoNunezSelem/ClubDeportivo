8687e3ecbff015f2c86e19694fc94b9e9e4b06f5
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-21 12:35:03 -0300

    UI form index - beta

---
+++ CapaUsuario/Inicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CapaUsuario
+{
+    public partial class Inicio : NavBar
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
