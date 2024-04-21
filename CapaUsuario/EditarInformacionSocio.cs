0982978a91f66ae01f83bad58658db12f1960b92
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-21 12:33:41 -0300

    centrar form

---
+++ CapaUsuario/EditarInformacionSocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CapaUsuario
+{
+    public partial class EditarInformacionSocio : CapaUsuario.NavBar
+    {
+        public EditarInformacionSocio()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
