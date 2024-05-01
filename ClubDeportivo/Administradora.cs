d5efdf261a8fcd1a0c4e57f5adb5c5c6eebbe972
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-30 22:16:22 -0300

    metodos base para socios de administradora

--- ClubDeportivo/Administradora.cs
+++ ClubDeportivo/Administradora.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClubDeportivo
+namespace CapaNegocio
 {
+    
     public class Administradora
     {
+        private List<Socio> socios;
+        public Administradora()
+        {
+            socios = new List<Socio>();
+        }
+
+        public Socio buscarSocio(string dni)
+        {
+            Socio socioEncontrado = socios.Find(socio => socio.esDni(dni));
+            return socioEncontrado;
+        }
+
+        public void agregarSocio(Socio socio) 
+        {
+            socios.Add(socio);
+        }
+
+        public void mostrarSocios()
+        {
+            foreach(Socio socio in socios)
+            {
+                string nom = socio.getNombre();
+                Console.WriteLine("El nombre del socio es: " + nom);
+            }
+        }
     }
 }
