a7edd4e02980852ebbbc741ee3d70f7fabc1fccb
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-30 22:14:32 -0300

    obtener info usuario

--- CapaUsuario/AgregarNuevoSocio.cs
+++ CapaUsuario/AgregarNuevoSocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,6 +12,7 @@ namespace CapaUsuario
 {
     public partial class AgregarNuevoSocio : CapaUsuario.NavBar
     {
+        private Socio socio;
         public AgregarNuevoSocio()
         {
             InitializeComponent();
@@ -19,7 +21,7 @@ namespace CapaUsuario
 
         private void MaskedTextBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            maskedTextBox1.Select(0, 0);
+            inputDni.Select(0, 0);
         }
 
         private void maskedTextBox2_MouseClick(object sender, MouseEventArgs e)
@@ -49,7 +51,20 @@ namespace CapaUsuario
 
         private void registrarUsuario_MouseClick(object sender, MouseEventArgs e)
         {
+            string dni = inputDni.Text;
+            string nombre = inputNombre.Text;
+            string apellido = inputApellido.Text;
+            string email = inputEmail.Text;
+            DateTime fechaNacimiento = DateTime.Parse(inputFechaNacimiento.Text);
 
+            this.socio = new Socio(dni,nombre,apellido,email,fechaNacimiento);
+
+            this.Close();
+        }
+
+        public Socio getSocio()
+        {
+            return this.socio;
         }
     }
 }
