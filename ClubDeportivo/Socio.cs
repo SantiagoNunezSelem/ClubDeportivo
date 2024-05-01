1d08cbb21819532651d1dd6105a77299ce0e7349
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-30 22:16:30 -0300

    Create Socio.cs

---
+++ ClubDeportivo/Socio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaNegocio
+{
+    public class Socio
+    {
+        private string dni;
+        private string nombre;
+        private string apellido;
+        private string email;
+        private DateTime fechaNacimiento;
+
+        public Socio(string dni, string nombre, string apellido, string email, DateTime fechaNacimiento)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public bool esDni(string dni)
+        {
+            return dni == this.dni;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+    }
+}
