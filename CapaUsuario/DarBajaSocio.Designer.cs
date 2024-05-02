1127051b9807776902d610258ad7d4c3205b7d1d
Santiago N.S <108593761+SantiagoNunezSelem@users.noreply.github.com>
2024-05-01 21:30:55 -0300

    Dar de Baja Socio (#3)
    * Dar Baja Socio hereda del formulario Información Socio

--- CapaUsuario/DarBajaSocio.Designer.cs
+++ CapaUsuario/DarBajaSocio.Designer.cs
@@ -28,250 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelEmail = new System.Windows.Forms.Label();
-            this.labelTelefono = new System.Windows.Forms.Label();
-            this.labelFechaNacimiento = new System.Windows.Forms.Label();
-            this.labelApellido = new System.Windows.Forms.Label();
-            this.labelNombre = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.buscarDni = new System.Windows.Forms.Button();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.InputBuscarDni = new System.Windows.Forms.MaskedTextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.registrarUsuario = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.botonEliminar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // labelEmail
+            // botonEliminar
             // 
-            this.labelEmail.AutoSize = true;
-            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.labelEmail.Location = new System.Drawing.Point(508, 243);
-            this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(92, 17);
-            this.labelEmail.TabIndex = 50;
-            this.labelEmail.Text = "Info del Email";
+            this.botonEliminar.Location = new System.Drawing.Point(441, 288);
+            this.botonEliminar.Name = "botonEliminar";
+            this.botonEliminar.Size = new System.Drawing.Size(137, 23);
+            this.botonEliminar.TabIndex = 35;
+            this.botonEliminar.Text = "Eliminar";
+            this.botonEliminar.UseVisualStyleBackColor = true;
+            this.botonEliminar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.botonEliminar_MouseClick);
             // 
-            // labelTelefono
-            // 
-            this.labelTelefono.AutoSize = true;
-            this.labelTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.labelTelefono.Location = new System.Drawing.Point(508, 182);
-            this.labelTelefono.Name = "labelTelefono";
-            this.labelTelefono.Size = new System.Drawing.Size(114, 17);
-            this.labelTelefono.TabIndex = 49;
-            this.labelTelefono.Text = "Info del Telefono";
-            // 
-            // labelFechaNacimiento
-            // 
-            this.labelFechaNacimiento.AutoSize = true;
-            this.labelFechaNacimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.labelFechaNacimiento.Location = new System.Drawing.Point(148, 330);
-            this.labelFechaNacimiento.Name = "labelFechaNacimiento";
-            this.labelFechaNacimiento.Size = new System.Drawing.Size(148, 17);
-            this.labelFechaNacimiento.TabIndex = 48;
-            this.labelFechaNacimiento.Text = "Info Fecha Nacimiento";
-            // 
-            // labelApellido
-            // 
-            this.labelApellido.AutoSize = true;
-            this.labelApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.labelApellido.Location = new System.Drawing.Point(148, 252);
-            this.labelApellido.Name = "labelApellido";
-            this.labelApellido.Size = new System.Drawing.Size(108, 17);
-            this.labelApellido.TabIndex = 47;
-            this.labelApellido.Text = "Info del Apellido";
-            // 
-            // labelNombre
-            // 
-            this.labelNombre.AutoSize = true;
-            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.labelNombre.Location = new System.Drawing.Point(148, 182);
-            this.labelNombre.Name = "labelNombre";
-            this.labelNombre.Size = new System.Drawing.Size(108, 17);
-            this.labelNombre.TabIndex = 46;
-            this.labelNombre.Text = "Info del Nombre";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label7.Location = new System.Drawing.Point(439, 243);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 45;
-            this.label7.Text = "Email:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label8.Location = new System.Drawing.Point(423, 182);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(68, 17);
-            this.label8.TabIndex = 44;
-            this.label8.Text = "Teléfono:";
-            // 
-            // buscarDni
-            // 
-            this.buscarDni.Location = new System.Drawing.Point(243, 121);
-            this.buscarDni.Name = "buscarDni";
-            this.buscarDni.Size = new System.Drawing.Size(75, 23);
-            this.buscarDni.TabIndex = 43;
-            this.buscarDni.Text = "Buscar";
-            this.buscarDni.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::CapaUsuario.Properties.Resources.pngwing_com;
-            this.pictureBox2.Location = new System.Drawing.Point(135, 140);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(96, 11);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 42;
-            this.pictureBox2.TabStop = false;
-            // 
-            // InputBuscarDni
-            // 
-            this.InputBuscarDni.AccessibleDescription = "";
-            this.InputBuscarDni.AccessibleName = "";
-            this.InputBuscarDni.AllowPromptAsInput = false;
-            this.InputBuscarDni.BackColor = System.Drawing.SystemColors.Control;
-            this.InputBuscarDni.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.InputBuscarDni.CausesValidation = false;
-            this.InputBuscarDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.InputBuscarDni.ForeColor = System.Drawing.Color.Black;
-            this.InputBuscarDni.Location = new System.Drawing.Point(137, 123);
-            this.InputBuscarDni.Mask = "99999999";
-            this.InputBuscarDni.Name = "InputBuscarDni";
-            this.InputBuscarDni.RejectInputOnFirstFailure = true;
-            this.InputBuscarDni.ResetOnPrompt = false;
-            this.InputBuscarDni.ResetOnSpace = false;
-            this.InputBuscarDni.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.InputBuscarDni.ShortcutsEnabled = false;
-            this.InputBuscarDni.Size = new System.Drawing.Size(100, 16);
-            this.InputBuscarDni.SkipLiterals = false;
-            this.InputBuscarDni.TabIndex = 41;
-            this.InputBuscarDni.Tag = "";
-            this.InputBuscarDni.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
-            this.InputBuscarDni.ValidatingType = typeof(int);
-            this.InputBuscarDni.MouseClick += new System.Windows.Forms.MouseEventHandler(this.InputBuscarDni_MouseClick);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label6.Location = new System.Drawing.Point(50, 313);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 17);
-            this.label6.TabIndex = 40;
-            this.label6.Text = "Fecha de";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label5.Location = new System.Drawing.Point(48, 330);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 17);
-            this.label5.TabIndex = 39;
-            this.label5.Text = "Nacimiento:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label4.Location = new System.Drawing.Point(69, 252);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 17);
-            this.label4.TabIndex = 38;
-            this.label4.Text = "Apellido:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label3.Location = new System.Drawing.Point(71, 182);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 17);
-            this.label3.TabIndex = 37;
-            this.label3.Text = "Nombre:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label2.Location = new System.Drawing.Point(98, 123);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 17);
-            this.label2.TabIndex = 36;
-            this.label2.Text = "DNI:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label1.Location = new System.Drawing.Point(33, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 20);
-            this.label1.TabIndex = 35;
-            this.label1.Text = "Dar de Baja Socio";
-            // 
-            // registrarUsuario
-            // 
-            this.registrarUsuario.Location = new System.Drawing.Point(461, 288);
-            this.registrarUsuario.Name = "registrarUsuario";
-            this.registrarUsuario.Size = new System.Drawing.Size(96, 23);
-            this.registrarUsuario.TabIndex = 51;
-            this.registrarUsuario.Text = "Eliminar Socio";
-            this.registrarUsuario.UseVisualStyleBackColor = true;
-            // 
-            // DarBajaSocio
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.registrarUsuario);
-            this.Controls.Add(this.labelEmail);
-            this.Controls.Add(this.labelTelefono);
-            this.Controls.Add(this.labelFechaNacimiento);
-            this.Controls.Add(this.labelApellido);
-            this.Controls.Add(this.labelNombre);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.buscarDni);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.InputBuscarDni);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "DarBajaSocio";
-            this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
-            this.Controls.SetChildIndex(this.label3, 0);
-            this.Controls.SetChildIndex(this.label4, 0);
-            this.Controls.SetChildIndex(this.label5, 0);
-            this.Controls.SetChildIndex(this.label6, 0);
-            this.Controls.SetChildIndex(this.InputBuscarDni, 0);
-            this.Controls.SetChildIndex(this.pictureBox2, 0);
-            this.Controls.SetChildIndex(this.buscarDni, 0);
-            this.Controls.SetChildIndex(this.label8, 0);
-            this.Controls.SetChildIndex(this.label7, 0);
-            this.Controls.SetChildIndex(this.labelNombre, 0);
-            this.Controls.SetChildIndex(this.labelApellido, 0);
-            this.Controls.SetChildIndex(this.labelFechaNacimiento, 0);
-            this.Controls.SetChildIndex(this.labelTelefono, 0);
-            this.Controls.SetChildIndex(this.labelEmail, 0);
-            this.Controls.SetChildIndex(this.registrarUsuario, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Controls.Add(this.botonEliminar);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.SetChildIndex(this.botonEliminar, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -279,22 +56,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label labelEmail;
-        private System.Windows.Forms.Label labelTelefono;
-        private System.Windows.Forms.Label labelFechaNacimiento;
-        private System.Windows.Forms.Label labelApellido;
-        private System.Windows.Forms.Label labelNombre;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button buscarDni;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.MaskedTextBox InputBuscarDni;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button registrarUsuario;
+        private System.Windows.Forms.Button botonEliminar;
     }
 }
--- CapaUsuario/DarBajaSocio.cs
+++ CapaUsuario/DarBajaSocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,17 +9,31 @@ using System.Windows.Forms;
 
 namespace CapaUsuario
 {
-    public partial class DarBajaSocio : CapaUsuario.NavBar
+    public partial class DarBajaSocio : CapaUsuario.InformacionSocio
     {
         public DarBajaSocio()
         {
             InitializeComponent();
-            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
-        private void InputBuscarDni_MouseClick(object sender, MouseEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            InputBuscarDni.Select(0, 0);
+
+        }
+
+        private void botonEliminar_MouseClick(object sender, MouseEventArgs e)
+        {
+            string dniBuscar = InputBuscarDni.Text;
+            Socio buscarSocio = base.existeSocio(dniBuscar);
+            if (buscarSocio != null)
+            {
+                base.eliminarSocio(dniBuscar);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Persona no registrada", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
