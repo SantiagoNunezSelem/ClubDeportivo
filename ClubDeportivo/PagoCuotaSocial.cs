a0ba5edb10c40fe33b5f989668bc26cd15e87ae2
Santiago N.S. <gigogisantisd@gmail.com>
2024-05-06 00:10:17 -0300

    Create PagoCuotaSocial.cs

---
+++ ClubDeportivo/PagoCuotaSocial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    internal class PagoCuotaSocial : Pago
+    {
+        private decimal PRECIOMENSUAL;
+        private int MAXACTIVIDADESGRATUITAS;
+        private int cantActividadesDeCuotaSocial; //Cant actividades registradas cuando la cuota social esta activa
+        private int cantMeses;
+        
+
+        public PagoCuotaSocial(Socio socio, decimal pagoFinal, DateTime fechaPago, int cantMeses)
+            : base(socio, pagoFinal, fechaPago)
+        {
+            this.PRECIOMENSUAL = 5000;
+            this.MAXACTIVIDADESGRATUITAS = 5;
+            this.cantActividadesDeCuotaSocial = 0;
+            this.cantMeses = cantMeses;
+        }
+    }
+}
