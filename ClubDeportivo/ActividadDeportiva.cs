e0fe8fa75f636287b6e3e08626097d3f844212fe
Santiago N.S. <gigogisantisd@gmail.com>
2024-05-06 00:09:35 -0300

    Create ActividadDeportiva.cs

---
+++ ClubDeportivo/ActividadDeportiva.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    internal class ActividadDeportiva
+    {
+        string nombreProfesor;
+        string horario;
+        int cantAlumnosMax;
+        decimal precioMes;
+        private List<Socio> alumnosInscriptos;
+
+        public ActividadDeportiva(string nombreProfesor, string horario, int cantAlumnosMax, decimal precioMes)
+        {
+            this.nombreProfesor = nombreProfesor;
+            this.horario = horario;
+            this.cantAlumnosMax = cantAlumnosMax;
+            this.precioMes = precioMes;
+            alumnosInscriptos = new List<Socio>();
+        }
+    }
+}
