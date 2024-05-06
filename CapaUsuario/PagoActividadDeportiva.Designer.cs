a20cd782386109db70c774ef5d0dc7206870f8b3
Santiago N.S. <gigogisantisd@gmail.com>
2024-05-06 02:18:16 -0300

    Update PagoActividadDeportiva.Designer.cs

--- CapaUsuario/PagoActividadDeportiva.Designer.cs
+++ CapaUsuario/PagoActividadDeportiva.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.inputApellido = new System.Windows.Forms.Label();
+            this.inputNombre = new System.Windows.Forms.Label();
+            this.inputPrecioFinal = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.inputCantMeses = new System.Windows.Forms.MaskedTextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.inputPrecioMes = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.guardarPagoActividadDeportiva = new System.Windows.Forms.Button();
-            this.buscarDni = new System.Windows.Forms.Button();
+            this.botonBuscarDni = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.inputDniBuscar = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
@@ -51,7 +51,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label13 = new System.Windows.Forms.Label();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.selecActividadDeportiva = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -61,35 +61,32 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
-            // label11
+            // inputApellido
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label11.Location = new System.Drawing.Point(117, 249);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(120, 17);
-            this.label11.TabIndex = 81;
-            this.label11.Text = "Apellido del Socio";
+            this.inputApellido.AutoSize = true;
+            this.inputApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.inputApellido.Location = new System.Drawing.Point(117, 249);
+            this.inputApellido.Name = "inputApellido";
+            this.inputApellido.Size = new System.Drawing.Size(0, 17);
+            this.inputApellido.TabIndex = 81;
             // 
-            // label10
+            // inputNombre
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label10.Location = new System.Drawing.Point(117, 179);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(120, 17);
-            this.label10.TabIndex = 80;
-            this.label10.Text = "Nombre del Socio";
+            this.inputNombre.AutoSize = true;
+            this.inputNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.inputNombre.Location = new System.Drawing.Point(117, 179);
+            this.inputNombre.Name = "inputNombre";
+            this.inputNombre.Size = new System.Drawing.Size(0, 17);
+            this.inputNombre.TabIndex = 80;
             // 
-            // label7
+            // inputPrecioFinal
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label7.Location = new System.Drawing.Point(520, 260);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(56, 17);
-            this.label7.TabIndex = 79;
-            this.label7.Text = "$50000";
+            this.inputPrecioFinal.AutoSize = true;
+            this.inputPrecioFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.inputPrecioFinal.Location = new System.Drawing.Point(520, 260);
+            this.inputPrecioFinal.Name = "inputPrecioFinal";
+            this.inputPrecioFinal.Size = new System.Drawing.Size(0, 17);
+            this.inputPrecioFinal.TabIndex = 79;
             // 
             // label9
             // 
@@ -124,6 +121,8 @@
             this.inputCantMeses.TabIndex = 75;
             this.inputCantMeses.Tag = "";
             this.inputCantMeses.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            this.inputCantMeses.TypeValidationCompleted += new System.Windows.Forms.TypeValidationEventHandler(this.inputCantMeses_TypeValidationCompleted);
+            this.inputCantMeses.Validating += new System.ComponentModel.CancelEventHandler(this.inputCantMeses_Validating);
             // 
             // label8
             // 
@@ -135,15 +134,15 @@
             this.label8.TabIndex = 74;
             this.label8.Text = "Cantidad de Meses:";
             // 
-            // label6
+            // inputPrecioMes
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label6.Location = new System.Drawing.Point(525, 148);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(48, 17);
-            this.label6.TabIndex = 73;
-            this.label6.Text = "$5000";
+            this.inputPrecioMes.AutoSize = true;
+            this.inputPrecioMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.inputPrecioMes.Location = new System.Drawing.Point(525, 148);
+            this.inputPrecioMes.Name = "inputPrecioMes";
+            this.inputPrecioMes.Size = new System.Drawing.Size(49, 17);
+            this.inputPrecioMes.TabIndex = 73;
+            this.inputPrecioMes.Text = "         -";
             // 
             // label5
             // 
@@ -163,15 +162,17 @@
             this.guardarPagoActividadDeportiva.TabIndex = 70;
             this.guardarPagoActividadDeportiva.Text = "Guardar";
             this.guardarPagoActividadDeportiva.UseVisualStyleBackColor = true;
+            this.guardarPagoActividadDeportiva.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guardarPagoActividadDeportiva_MouseClick);
             // 
-            // buscarDni
+            // botonBuscarDni
             // 
-            this.buscarDni.Location = new System.Drawing.Point(226, 107);
-            this.buscarDni.Name = "buscarDni";
-            this.buscarDni.Size = new System.Drawing.Size(75, 23);
-            this.buscarDni.TabIndex = 69;
-            this.buscarDni.Text = "Buscar";
-            this.buscarDni.UseVisualStyleBackColor = true;
+            this.botonBuscarDni.Location = new System.Drawing.Point(226, 107);
+            this.botonBuscarDni.Name = "botonBuscarDni";
+            this.botonBuscarDni.Size = new System.Drawing.Size(75, 23);
+            this.botonBuscarDni.TabIndex = 69;
+            this.botonBuscarDni.Text = "Buscar";
+            this.botonBuscarDni.UseVisualStyleBackColor = true;
+            this.botonBuscarDni.MouseClick += new System.Windows.Forms.MouseEventHandler(this.botonBuscarDni_MouseClick);
             // 
             // label4
             // 
@@ -203,30 +204,30 @@
             this.label2.TabIndex = 64;
             this.label2.Text = "DNI:";
             // 
-            // maskedTextBox1
+            // inputDniBuscar
             // 
-            this.maskedTextBox1.AccessibleDescription = "";
-            this.maskedTextBox1.AccessibleName = "";
-            this.maskedTextBox1.AllowPromptAsInput = false;
-            this.maskedTextBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.maskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.maskedTextBox1.CausesValidation = false;
-            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.maskedTextBox1.ForeColor = System.Drawing.Color.Black;
-            this.maskedTextBox1.Location = new System.Drawing.Point(120, 109);
-            this.maskedTextBox1.Mask = "99999999";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.RejectInputOnFirstFailure = true;
-            this.maskedTextBox1.ResetOnPrompt = false;
-            this.maskedTextBox1.ResetOnSpace = false;
-            this.maskedTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.maskedTextBox1.ShortcutsEnabled = false;
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 16);
-            this.maskedTextBox1.SkipLiterals = false;
-            this.maskedTextBox1.TabIndex = 62;
-            this.maskedTextBox1.Tag = "";
-            this.maskedTextBox1.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
-            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.inputDniBuscar.AccessibleDescription = "";
+            this.inputDniBuscar.AccessibleName = "";
+            this.inputDniBuscar.AllowPromptAsInput = false;
+            this.inputDniBuscar.BackColor = System.Drawing.SystemColors.Control;
+            this.inputDniBuscar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputDniBuscar.CausesValidation = false;
+            this.inputDniBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.inputDniBuscar.ForeColor = System.Drawing.Color.Black;
+            this.inputDniBuscar.Location = new System.Drawing.Point(120, 109);
+            this.inputDniBuscar.Mask = "99999999";
+            this.inputDniBuscar.Name = "inputDniBuscar";
+            this.inputDniBuscar.RejectInputOnFirstFailure = true;
+            this.inputDniBuscar.ResetOnPrompt = false;
+            this.inputDniBuscar.ResetOnSpace = false;
+            this.inputDniBuscar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.inputDniBuscar.ShortcutsEnabled = false;
+            this.inputDniBuscar.Size = new System.Drawing.Size(100, 16);
+            this.inputDniBuscar.SkipLiterals = false;
+            this.inputDniBuscar.TabIndex = 62;
+            this.inputDniBuscar.Tag = "";
+            this.inputDniBuscar.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            this.inputDniBuscar.ValidatingType = typeof(int);
             // 
             // label1
             // 
@@ -318,71 +319,72 @@
             this.pictureBox7.TabIndex = 82;
             this.pictureBox7.TabStop = false;
             // 
-            // comboBox1
+            // selecActividadDeportiva
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.selecActividadDeportiva.FormattingEnabled = true;
+            this.selecActividadDeportiva.Items.AddRange(new object[] {
             "Natacion",
             "Futbol",
             "Voley"});
-            this.comboBox1.Location = new System.Drawing.Point(523, 88);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(96, 21);
-            this.comboBox1.TabIndex = 84;
+            this.selecActividadDeportiva.Location = new System.Drawing.Point(523, 88);
+            this.selecActividadDeportiva.Name = "selecActividadDeportiva";
+            this.selecActividadDeportiva.Size = new System.Drawing.Size(96, 21);
+            this.selecActividadDeportiva.TabIndex = 84;
+            this.selecActividadDeportiva.SelectedIndexChanged += new System.EventHandler(this.selecActividadDeportiva_SelectedIndexChanged);
             // 
             // PagoActividadDeportiva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.selecActividadDeportiva);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.inputApellido);
+            this.Controls.Add(this.inputNombre);
+            this.Controls.Add(this.inputPrecioFinal);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.inputCantMeses);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.inputPrecioMes);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.guardarPagoActividadDeportiva);
-            this.Controls.Add(this.buscarDni);
+            this.Controls.Add(this.botonBuscarDni);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.inputDniBuscar);
             this.Controls.Add(this.label1);
             this.Name = "PagoActividadDeportiva";
             this.Controls.SetChildIndex(this.label1, 0);
-            this.Controls.SetChildIndex(this.maskedTextBox1, 0);
+            this.Controls.SetChildIndex(this.inputDniBuscar, 0);
             this.Controls.SetChildIndex(this.pictureBox2, 0);
             this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.pictureBox1, 0);
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.pictureBox3, 0);
             this.Controls.SetChildIndex(this.label4, 0);
-            this.Controls.SetChildIndex(this.buscarDni, 0);
+            this.Controls.SetChildIndex(this.botonBuscarDni, 0);
             this.Controls.SetChildIndex(this.guardarPagoActividadDeportiva, 0);
             this.Controls.SetChildIndex(this.pictureBox4, 0);
             this.Controls.SetChildIndex(this.label5, 0);
-            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.inputPrecioMes, 0);
             this.Controls.SetChildIndex(this.label8, 0);
             this.Controls.SetChildIndex(this.inputCantMeses, 0);
             this.Controls.SetChildIndex(this.pictureBox5, 0);
             this.Controls.SetChildIndex(this.pictureBox6, 0);
             this.Controls.SetChildIndex(this.label9, 0);
-            this.Controls.SetChildIndex(this.label7, 0);
-            this.Controls.SetChildIndex(this.label10, 0);
-            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.inputPrecioFinal, 0);
+            this.Controls.SetChildIndex(this.inputNombre, 0);
+            this.Controls.SetChildIndex(this.inputApellido, 0);
             this.Controls.SetChildIndex(this.pictureBox7, 0);
             this.Controls.SetChildIndex(this.label13, 0);
-            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.selecActividadDeportiva, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -397,29 +399,29 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label inputApellido;
+        private System.Windows.Forms.Label inputNombre;
+        private System.Windows.Forms.Label inputPrecioFinal;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.MaskedTextBox inputCantMeses;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label inputPrecioMes;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Button guardarPagoActividadDeportiva;
-        private System.Windows.Forms.Button buscarDni;
+        private System.Windows.Forms.Button botonBuscarDni;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox inputDniBuscar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox selecActividadDeportiva;
     }
 }
