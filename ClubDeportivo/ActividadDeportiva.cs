3e5c4003888250a1bb6e666cbc9e6b47500e5baf
Santiago N.S. <gigogisantisd@gmail.com>
2024-05-06 02:18:25 -0300

    Update ActividadDeportiva.cs

--- ClubDeportivo/ActividadDeportiva.cs
+++ ClubDeportivo/ActividadDeportiva.cs
@@ -1,26 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class ActividadDeportiva
+    public class ActividadDeportiva
     {
+        string nombreActividad;
         string nombreProfesor;
         string horario;
         int cantAlumnosMax;
         decimal precioMes;
         private List<Socio> alumnosInscriptos;
 
-        public ActividadDeportiva(string nombreProfesor, string horario, int cantAlumnosMax, decimal precioMes)
+        public ActividadDeportiva(string nombreActividad, string nombreProfesor, string horario, int cantAlumnosMax, decimal precioMes)
         {
+            this.nombreActividad = nombreActividad;
             this.nombreProfesor = nombreProfesor;
             this.horario = horario;
             this.cantAlumnosMax = cantAlumnosMax;
             this.precioMes = precioMes;
             alumnosInscriptos = new List<Socio>();
+        }
+
+        public string Nombre
+        {
+            get { return nombreActividad; }
+            set { this.nombreActividad = value; }
+        }
+
+        public decimal PrecioMes
+        {
+            get { return precioMes; }
+            set { this.precioMes = value; }
         }
     }
 }
