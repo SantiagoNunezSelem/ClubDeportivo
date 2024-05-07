c235f931b50b93860fa6a55af94db233d9b3b5b0
ArielEstudios <ariel.ratoneando.estudios@gmail.com>
2024-05-07 20:08:47 -0300

    test: prueba

--- CapaUsuario/Program.cs
+++ CapaUsuario/Program.cs
@@ -15,6 +15,8 @@ namespace CapaUsuario
         [STAThread]
         static void Main()
         {
+
+            /// PRUEBA
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Inicio());
