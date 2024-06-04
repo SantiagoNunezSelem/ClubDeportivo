1105cfeb42ccf7647968fc099309bafcb5a13578
Santiago N.S <108593761+SantiagoNunezSelem@users.noreply.github.com>
2024-06-04 20:10:32 -0300

    Mostrar información de actividades deportivas (#15)

--- CapaUsuario/InformacionActividadDeportiva.Designer.cs
+++ CapaUsuario/InformacionActividadDeportiva.Designer.cs
@@ -28,28 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.selectActividadDeportiva = new System.Windows.Forms.ComboBox();
             this.label13 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.labelPrecioPorMes = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.labelVacantes = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelAluInscriptos = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelHorario = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // selectActividadDeportiva
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.selectActividadDeportiva.FormattingEnabled = true;
+            this.selectActividadDeportiva.Items.AddRange(new object[] {
             "Natacion",
             "Futbol",
             "Voley"});
-            this.comboBox1.Location = new System.Drawing.Point(194, 99);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(96, 21);
-            this.comboBox1.TabIndex = 97;
+            this.selectActividadDeportiva.Location = new System.Drawing.Point(194, 99);
+            this.selectActividadDeportiva.Name = "selectActividadDeportiva";
+            this.selectActividadDeportiva.Size = new System.Drawing.Size(96, 21);
+            this.selectActividadDeportiva.TabIndex = 97;
+            this.selectActividadDeportiva.SelectedIndexChanged += new System.EventHandler(this.selectActividadDeportiva_SelectedIndexChanged);
             // 
             // label13
             // 
@@ -61,15 +74,14 @@
             this.label13.TabIndex = 96;
             this.label13.Text = "Actividad Deportiva:";
             // 
-            // label6
+            // labelPrecioPorMes
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.label6.Location = new System.Drawing.Point(196, 159);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(48, 17);
-            this.label6.TabIndex = 88;
-            this.label6.Text = "$5000";
+            this.labelPrecioPorMes.AutoSize = true;
+            this.labelPrecioPorMes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelPrecioPorMes.Location = new System.Drawing.Point(196, 159);
+            this.labelPrecioPorMes.Name = "labelPrecioPorMes";
+            this.labelPrecioPorMes.Size = new System.Drawing.Size(0, 17);
+            this.labelPrecioPorMes.TabIndex = 88;
             // 
             // label5
             // 
@@ -111,27 +123,119 @@
             this.label1.TabIndex = 98;
             this.label1.Text = "Información de Actividad Deportiva";
             // 
+            // labelVacantes
+            // 
+            this.labelVacantes.AutoSize = true;
+            this.labelVacantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelVacantes.Location = new System.Drawing.Point(196, 227);
+            this.labelVacantes.Name = "labelVacantes";
+            this.labelVacantes.Size = new System.Drawing.Size(0, 17);
+            this.labelVacantes.TabIndex = 101;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(118, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 100;
+            this.label3.Text = "Vacantes:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaUsuario.Properties.Resources.pngwing_com;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 247);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 11);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 99;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelAluInscriptos
+            // 
+            this.labelAluInscriptos.AutoSize = true;
+            this.labelAluInscriptos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelAluInscriptos.Location = new System.Drawing.Point(196, 300);
+            this.labelAluInscriptos.Name = "labelAluInscriptos";
+            this.labelAluInscriptos.Size = new System.Drawing.Size(0, 17);
+            this.labelAluInscriptos.TabIndex = 104;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label7.Location = new System.Drawing.Point(59, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 17);
+            this.label7.TabIndex = 103;
+            this.label7.Text = "Alumnos Inscriptos:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CapaUsuario.Properties.Resources.pngwing_com;
+            this.pictureBox2.Location = new System.Drawing.Point(194, 320);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 11);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 102;
+            this.pictureBox2.TabStop = false;
+            // 
+            // labelHorario
+            // 
+            this.labelHorario.AutoSize = true;
+            this.labelHorario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.labelHorario.Location = new System.Drawing.Point(196, 362);
+            this.labelHorario.Name = "labelHorario";
+            this.labelHorario.Size = new System.Drawing.Size(0, 17);
+            this.labelHorario.TabIndex = 107;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(128, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 106;
+            this.label4.Text = "Horario:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CapaUsuario.Properties.Resources.pngwing_com;
+            this.pictureBox3.Location = new System.Drawing.Point(194, 382);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(96, 11);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 105;
+            this.pictureBox3.TabStop = false;
+            // 
             // InformacionActividadDeportiva
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelHorario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.labelAluInscriptos);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelVacantes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.selectActividadDeportiva);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labelPrecioPorMes);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox4);
             this.Name = "InformacionActividadDeportiva";
-            this.Controls.SetChildIndex(this.pictureBox4, 0);
-            this.Controls.SetChildIndex(this.label5, 0);
-            this.Controls.SetChildIndex(this.label6, 0);
-            this.Controls.SetChildIndex(this.pictureBox7, 0);
-            this.Controls.SetChildIndex(this.label13, 0);
-            this.Controls.SetChildIndex(this.comboBox1, 0);
-            this.Controls.SetChildIndex(this.label1, 0);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,12 +243,21 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox selectActividadDeportiva;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelPrecioPorMes;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelVacantes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelAluInscriptos;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelHorario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
